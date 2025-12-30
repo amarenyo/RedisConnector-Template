5a210540575a5ab177d26a37f55d431da508aec9
Enyo Fuhrmann <amarenyo@gmail.com>
2025-12-30 11:42:59 +0100

    added Redis to build.gradle, added test code for redis

--- src/main/java/ch/amarenyo/redisconnector/Redisconnector.java
+++ src/main/java/ch/amarenyo/redisconnector/Redisconnector.java
@@ -1,17 +1,34 @@
 package ch.amarenyo.redisconnector;
 
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Redisconnector extends JavaPlugin {
+
+    RedisURI uri = RedisURI.Builder
+            .redis("localhost", 6379)
+            .build();
+
+    RedisClient client = RedisClient.create(uri);
+    StatefulRedisConnection<String, String> connection = client.connect();
+    RedisCommands<String, String> commands = connection.sync();
 
     @Override
     public void onEnable() {
         System.out.println("RedisConnector has been changed; Plugin has been enabled!");
 
+        commands.set("hello", "world");
+        String result = commands.get("hello");
+        System.out.println(result);
+
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        connection.close();
+        client.shutdown();
     }
 }
